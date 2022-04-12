06f502aef65fcfad5e4968a3a5171daf5dfba163
umt.eng52@gmail.com <umt.eng52@gmail.com>
2022-04-12 13:35:36 +0700

    add template pdf

--- resources/views/mdca/show.blade.php
+++ resources/views/mdca/show.blade.php
@@ -2,5 +2,24 @@
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Carbon;
 use \Mpdf\Mpdf as MPDF;
+
+ini_set('max_execution_time', 300);
+$mpdf = new MPDF();
+
+$template = URL::asset('medicalca.pdf');
+$pagecount = $mpdf->SetSourceFile('../public/medicalca.pdf');
+$tplId = $mpdf->ImportPage($pagecount);
+$mpdf->SetPageTemplate($tplId);
+
+// Do not add page until page template set, as it is inserted at the start of each page
+$mpdf->AddPage();
+
+$mpdf->WriteHTML('Hello World');
+
+// The template $tplId will be inserted on all subsequent pages until (optionally)
+// $mpdf->SetPageTemplate();
+
+$mpdf->Output();
+
 @endphp
 
